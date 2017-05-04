4d362a92d6063e2c81277908312f98c8025f77ac
Irina Martyshko <ira.2995@mail.ru>
2017-05-04 10:15:16 +0300

    fixes

---
+++ src/public/app/main.js
@@ -0,0 +1,22 @@
+var common_path = "public";
+
+requirejs.config({
+    waitSeconds: 15,
+    shim: {
+        bootstrap: {
+            deps: ['jquery']
+        },
+    },
+    paths: {
+        jquery: "vendor/jquery-3.1.1.min",
+        bootstrap: "vendor/bootstrap.min",
+        wow: "vendor/wow",
+        slick: "vendor/slick/min",
+    },
+    baseUrl: common_path,
+    urlArgs: 'v=0.12'
+});
+
+requirejs(['app/common/script', 'app/common/design'], function(common, design) {
+    common.init();
+});
